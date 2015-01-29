7664bbf48da5590b0c589d87a471071cfa58bb2f
jaguillo <juloo.dsi@gmail.com>
2015-01-29 09:40:58 +0100

    ex02

---
+++ ex02/ft_range.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/01/29 09:25:29 by exam              #+#    #+#             */
+/*   Updated: 2015/01/29 09:39:52 by exam             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int			*ft_range(int start, int end)
+{
+	int			*range;
+	int			i;
+
+	if (start > end)
+		range = (int*)malloc(sizeof(int) * (start - end));
+	else
+		range = (int*)malloc(sizeof(int) * (end - start + 1));
+	i = 0;
+	while (start != end)
+	{
+		range[i++] = start;
+		start += (start > end) ? -1 : 1;
+	}
+	range[i] = start;
+	return (range);
+}
